21f3912721d065240a5b4986a1cca9b3e2f556ff
Lincxx <bossco28@gmail.com>
2025-01-21 10:00:30 -0500

    Add in query parameter with path parameter

--- books.py
+++ books.py
@@ -20,7 +20,7 @@ async def read_all_books():
 # async def read_all_books():
 #     return {'book title': 'My fav book'}
 
-
+# path parameters - ie http://localhost:8000/books/mybook
 @app.get("/books/{book_title}")
 async def read_book(book_title: str):
     for book in BOOKS:
@@ -28,3 +28,26 @@ async def read_book(book_title: str):
             return book
 
 
+
+# query parameters - ie http://localhost:8000/books/?category=math
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+# path and query parameters - ie http://localhost:8000/books/author_name/?category=category
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+           book.get('category').casefold() == category.casefold() :
+            books_to_return.append(book)
+
+    return books_to_return
