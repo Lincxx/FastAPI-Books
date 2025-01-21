605e2e1e7240e88a6879a11cf0e2e520348779d6
Lincxx <bossco28@gmail.com>
2025-01-21 06:23:31 -0500

    Add in new end-point that return data of books

--- books.py
+++ books.py
@@ -2,6 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/api-endpoint") 
-async def fast_api():
-    return {'message': 'Hello, Jeremy'}
+BOOKS = [
+    {'title': 'titele 1', 'author': 'author 1', 'category': 'science'},
+    {'title': 'titele 2', 'author': 'author 2', 'category': 'science'},
+    {'title': 'titele 3', 'author': 'author 3', 'category': 'history'},
+    {'title': 'titele 4', 'author': 'author 4', 'category': 'math'},
+    {'title': 'titele 5', 'author': 'author 5', 'category': 'math'},
+    {'title': 'titele 6', 'author': 'author 2', 'category': 'math'},
+]
+
+
+@app.get("/books") 
+async def read_all_books():
+    return BOOKS
+
+
+# @app.get("/api-endpoint") 
+# async def fast_api():
+#     return BOOKS
