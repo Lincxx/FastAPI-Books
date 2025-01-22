3a311a745c55c655b3c83afb111b5b85443062c7
Lincxx <bossco28@gmail.com>
2025-01-22 07:37:53 -0500

    Add in post method

--- books.py
+++ books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body,FastAPI
 
 app = FastAPI()
 
@@ -51,3 +51,7 @@ async def read_author_category_by_query(book_author: str, category: str):
             books_to_return.append(book)
 
     return books_to_return
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body(...)): #, it's a special syntax in FastAPI that indicates the parameter is required
+    BOOKS.append(new_book)
